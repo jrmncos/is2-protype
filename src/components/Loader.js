0a677342c0eb904931a8cc595d523b170f28964b
jrmncos <gcostilla@gcostil-ltmch5x.internal.salesforce.com>
2021-05-05 20:55:07 -0300

    Notificacion ante un evento ok

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -80,29 +80,33 @@ export default function Loader({nivel, setLoadSimulation, setRenderResults}){
         <h1>Semana: {week}</h1>
       </div>
 
-
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">{"Use Google's location service?"}</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            Let Google help apps determine location. This means sending anonymous location data to
-            Google, even when no apps are running.
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            Disagree
-          </Button>
-          <Button onClick={handleClose} color="primary" autoFocus>
-            Agree
-          </Button>
-        </DialogActions>
-      </Dialog>
+      {
+        nivel.eventos.map((evento) => {
+          return(
+            <>
+              <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+              >
+                <DialogTitle id="alert-dialog-title">{"Evento importante"}</DialogTitle>
+                <DialogContent>
+                  <DialogContentText id="alert-dialog-description">
+                    {evento.descripcion}
+                  </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                  <Button onClick={handleClose} color="primary">
+                    Continuar Simulacion
+                  </Button>
+                </DialogActions>
+              </Dialog>
+            </>
+          )
+        })
+      }
+      
       
     </>
   )
