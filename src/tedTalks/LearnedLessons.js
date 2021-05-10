b98f0f01c4e873c42adac8638a43c975711f9a93
Santiago Galvan <santiagodgalvan@gmail.com>
2021-05-09 17:37:30 -0700

    added learned lessons

---
+++ src/tedTalks/LearnedLessons.js
@@ -0,0 +1,109 @@
+import React from "react";
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import {  Box, Divider } from "@material-ui/core";
+
+
+export default function LearnedLessons() {
+
+  return (
+    <>
+    <Grid
+      container
+      direction="column"
+      justify="flex-start"
+      alignItems="stretch"
+      spacing={2}
+    >
+    <Typography variant="h3" component="h2" gutterBottom>
+      Lecciones aprendidas de las charlas teds
+    </Typography>    
+    <Divider/>
+    
+    <Box m={1}/>
+    <Typography variant="h5" component="h2" gutterBottom >
+      La procrastinación y la gestión del tiempo
+    </Typography>
+
+    <Typography variant="subtitle1" gutterBottom align="justify"  >
+      Podemos relacionar la charla de Tim Urban con la gestión del tiempo en los proyectos, ya que Tim menciona que cuando debió hacer su tesis, debió cambiar su forma de organizar sus tareas para llegar a terminarla a tiempo. Pero luego, por causa de su procrastinación, comenzó a postergar las tareas ya programadas, haciendo que deba hacer todas las tareas que debió hacer en el transcurso de un año en solo 72 horas. Utilizando esta reflexión como ejemplo, podríamos decir que tener mucho tiempo sin tener deadlines intermedios, puede provocar que haya atrasos en la realización de tareas. Podría relacionarse con el método ágil Scrum, donde se planifican las tareas dentro de un sprint.
+    </Typography>
+
+    <Box m={1}/>
+    <Typography variant="h5" component="h2" gutterBottom >
+      El papel de Facebook en el Brexit y la amenaza a la democracia
+    </Typography>
+
+    <Typography variant="subtitle1" gutterBottom align="justify"  >
+      Mente Extendida y su sección “De la cognición y la mente”,  de los autores David Clark y David Chalmers, resumidamente habla que la información es una creencia cuando tiene un papel fundamental en la gente y no importa si viene de la misma mente o de un factor externo como por ejemplo medios digitales, periodísticos, etc. En este sentido, se puede relacionar la conferencia de la periodista Carole Cadwalladr cuando habla de que la mayoría de la gente de Ebbw Vale en el sur de Gales, votó para que el Reino Unido saliera de la Unión Europea. Las personas de esa ciudad creían rotundamente en la información que vieron en Facebook en relación con los inmigrantes procedentes de Europa continental y sobre que Turquía ingresaría a dicha comunidad política.
+    </Typography>
+
+    <Typography variant="subtitle1" gutterBottom align="justify"  >
+      Lo cierto es que Ebbw Vale, según Carole, tiene la tasa más baja de inmigración del Reino Unido. Entonces, por más que haya hechos concretos de que la EU haya financiado la creación de una universidad, modernización de carreteras, creación de una nueva línea de tren, entre otros proyectos urbanísticos, se puede concluir que la información procedente de esa red social jugó un papel fundamental en esa ciudad, en relación a la definición de su votación.
+    </Typography>
+    
+    <Box m={1}/>
+    <Typography variant="h5" component="h2" gutterBottom >
+      Metáforas de carrera y discurso de Steve Jobs
+    </Typography>
+
+    <Typography variant="subtitle1" gutterBottom align="justify"  >
+      El discurso de Steve Jobs se conecta con la metáfora de las estaciones: la carrera como ciclo, debido a que atraviesa varias etapas en su carrera. Steve dejó la universidad tras los seis primeros meses, pero después siguió divagando meses, antes de dejar por completo. 
+    </Typography>
+
+    <Typography variant="subtitle1" gutterBottom align="justify"  >
+      Decidió dejar la universidad ya que no sabía qué hacer con su vida, y empezó a confiar en que las cosas saldrían bien a pesar de sus miedos.
+    </Typography>
+
+    <Typography variant="subtitle1" gutterBottom align="justify"  >
+      Cuando dejó, comenzó a meterse en las clases que le parecían interesantes, dejando las obligatorias. Siguió todo el tiempo su curiosidad e intuición.
+    </Typography>
+
+    <Typography variant="subtitle1" gutterBottom align="justify"  >
+      Decidió atender al curso de caligrafía. Luego de diez años, diseñaron el primer ordenador Macintosh, y utilizo lo aprendido. 
+    </Typography>
+
+    <Typography variant="subtitle1" gutterBottom align="justify"  >
+      Se relaciona con la metáfora de la red: la carrera como encuentros y relaciones. Por medio de relacionarse con Woz ambos crearon Apple en la cochera de sus padres cuando tenían 20 años. Trabajaron bastante hasta que a los diez años Apple creció en empleados.
+    </Typography>
+
+    <Typography variant="subtitle1" gutterBottom align="justify"  >
+      Steve había cumplido 30 años y fue despedido de su propia empresa, justo había sido un año que habían lanzado Macintosh. No sabía qué hacer durante algunos meses. Tanta pasión tenía por lo que hacía, que lo llevó a comenzar de nuevo. Pasó del éxito a ser de nuevo un principiante, menos seguro de las cosas.
+    </Typography>
+
+    <Typography variant="subtitle1" gutterBottom align="justify"  >
+      Durante los siguientes cinco años, creó NeXT y Pixar (creó el primer largometraje animado por ordenador, Toy Story). Apple compra NeXT, Steve regresa a Apple.
+    </Typography>
+
+    <Typography variant="subtitle1" gutterBottom align="justify"  >
+      Se relaciona lo anterior también con la metáfora artesanal: la carrera como construcción, debido a que sin darse cuenta sigue construyendo su carrera de a poco, al principio pensó que había logrado todo al construir Macintosh. Pero luego cuando lo echaron de su propia empresa sin darse cuenta siguió pasando por varias etapas y vueltas en su vida hasta llegar a construir NeXT. Él asegura que nada de esto habría ocurrido si no le hubieran echado de Apple. Lo que le permitió soportar todo fue la pasión por lo que hacía, seguir su intuición, elecciones y acciones impulsadas por procesos internos.
+    </Typography>
+
+    <Typography variant="subtitle1" gutterBottom align="justify"  >
+     La metáfora de las estaciones está presente, con una de las etapas que fue cuando le diagnosticaron cáncer de páncreas (que resultó curable con una cirugía), le ayudó a tomar las grandes decisiones de su vida y le generó cambios en la manera de ver la vida.
+    </Typography>
+
+    <Typography variant="subtitle1" gutterBottom align="justify"  >
+      Por último se relaciona con la metáfora narrativa: la carrera como historia, debido a que explica por todo lo que fue pasando, los contextos internos y externos que influyeron en él hasta llegar lo que es ahora y darse cuenta que siempre siguió lo que le apasionaba.
+    </Typography>
+
+    <Box m={1}/>
+    <Typography variant="h5" component="h2" gutterBottom >
+      Los project managers y cómo los grandes líderes nos inspiran 
+    </Typography>
+
+    <Typography variant="subtitle1" gutterBottom align="justify"  >
+      En la charla TED: How great leaders inspire action, de Simon Sinek, Simon menciona lo siguiente: “Existen líderes y personas que lideran. Los líderes tienen una posición de poder o de autoridad, pero los que lideran, nos inspiran. Ya sea que se trate de individuos u organizaciones, seguimos a los que lideran, no porque tenemos que hacerlo, sino porque queremos hacerlo. Seguimos a quienes lideran, no por ellos, sino por nosotros mismos.
+    </Typography>
+
+    <Typography variant="subtitle1" gutterBottom align="justify"  >
+      Y son los que comienzan con el "¿Por qué?", los que tienen la habilidad de inspirar a quienes los rodean o de encontrar a otros que los inspiren”
+    </Typography>
+
+    <Typography variant="subtitle1" gutterBottom align="justify"  >
+      Este pasaje podemos relacionarlo con lo visto de gestión de proyectos en el PMBOK, específicamente podemos relacionarlo con el rol de un líder de proyecto, sus características, su esfera de influencia y los distintos estilos de liderazgo que dicho líder puede emplear. En el PMBOK se define que una de las habilidades que debe poseer un project manager es la de liderazgo, esto implica que el líder debe tener los conocimientos, habilidades y comportamientos necesarios para guiar, motivar y dirigir un equipo, para ayudar a una organización a alcanzar sus metas de negocio. Por lo tanto una de las lecciones que podemos aprender de la charla TED de Simon Sinek, es que un líder debe ser capaz de liderar a los miembros de un equipo, y poder inspirar tanto a los miembros del equipo como a aquellos que se encuentran dentro de su esfera de influencia. Además de que un líder debe ser capaz de emplear un estilo de liderazgo que más se arrime a lo que plantea Simon, acerca de lograr inspirar a los demás.
+    </Typography>
+    </Grid>
+    </>
+  )
+}
---
+++ src/tedTalks/TalksRelation.js
@@ -0,0 +1,87 @@
+import React from "react";
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import {  Box, Divider } from "@material-ui/core";
+import ImageZoom from "react-medium-image-zoom";
+import relations from '../images/relations.png';
+
+
+export default function TalksRelation() {
+
+  return (
+    <>
+    <Grid
+      container
+      direction="column"
+      justify="flex-start"
+      alignItems="stretch"
+      spacing={2}
+    >
+    <Typography variant="h3" component="h2" gutterBottom>
+      Relaciones entre las charlas teds
+    </Typography>    
+    <Divider/>
+
+    <Typography variant="h5" component="h2" gutterBottom style={{fontStyle: "italic", color:"#808080"}}>
+      El siguiente diagrama muestra las relaciones que supimos identificar entre las charlas Teds.
+    </Typography>
+    <ImageZoom
+      image={{
+        src: relations,
+        alt: 'Golden Gate Bridge',
+        className: 'img',
+        style: { width: '50%' }
+      }}
+      zoomImage={{
+        src: 'bridge-big.jpg',
+        alt: 'Golden Gate Bridge'
+      }}
+    />
+    
+    <Box m={1}/>
+    <Typography variant="h5" component="h2" gutterBottom >
+      1. Relación entre Jason Clarke, Celeste Headlee y Simon Sinek
+    </Typography>
+
+    <Typography variant="subtitle1" gutterBottom align="justify"  >
+      Por un lado, lo podemos relacionar con la charla ted: “10 formas para tener una mejor conversación”, de Celeste Headlee con “Embracing Change”, de Jason Clarke ya que en la charla ofrecida J. Clarke se  plantea cómo nos enfrentamos al cambio, las reacciones que tenemos sobre él, y cómo lo llevamos a cabo, y en la charla abordada por C. Headlee si bien se plantean una serie de reglas o consejos para tener una conversación, éstos resultan puntos de cambios para una o más personas. Es decir, si revisamos nuevamente esas reglas, notaremos que para poder tener una conversación real con otra persona, seguramente tengamos que realizar una serie de cambios en uno o más hábitos propios; por lo que está bueno tener presente la charla de J. Clarke, para saber cómo abordar esos cambios de hábitos.
+    </Typography>
+
+    <Typography variant="subtitle1" gutterBottom align="justify"  >
+      Por el otro, también podemos relacionar la charla de C. Headlee con la charla: “How great leaders inspire action”, de Simon Sinek. En su charla, S. Sinek, plantea que algunas organizaciones o líderes pueden inspirar mientras que otros no. Esto se debe a que una parte de ellos ha podido cambiar y evolucionar a la hora de comunicar: saben lo que hacen, cómo lo hacen y por qué lo hacen. Mientras que otros no. Y probablemente uno de los motivos por lo cual no han podido, es porque no saben cómo realizar ese cambio, ni que tan necesario puede ser para el otro poder entender su comportamiento.
+    </Typography>
+
+    <Typography variant="subtitle1" gutterBottom align="justify"  >
+      Por último, podemos relacionar “Embracing Change”, de Jason Clarke con la charla “How great leaders inspire action”, de Simon Sinek. En su charla, S. Sinek, plantea que algunas organizaciones o líderes pueden inspirar mientras que otros no. Esto se debe a que una parte de ellos ha podido cambiar y evolucionar a la hora de comunicar: saben lo que hacen, cómo lo hacen y por qué lo hacen. Y probablemente uno de los motivos por lo cual no han podido, es porque no saben cómo realizar ese cambio, ni que tan necesario puede ser para el otro poder entender su comportamiento.
+    </Typography>  
+
+    <Box m={1}/>
+    <Typography variant="h5" component="h2" gutterBottom >
+      2. Relación entre Tim Urban, Jason Clarke y Steve Jobs
+    </Typography>
+
+    <Typography variant="subtitle1" gutterBottom align="justify"  >
+      Podemos relacionar las charlas “Inside the mind of a master procrastinator” de Tim Urban con la charla “Embracing Change” de Jason Clarke y el discurso “Discurso de Graduación. Stanford University.” de Steve Jobs debido a que los autores mencionan que se debe hacer un cambio en los hábitos o sobre estos hábitos persee. Tim Urban habla sobre los hábitos de la gente, como estos pueden llevar a postergar tareas a causa de la procrastinación y cómo hay gente que se lleva bien con los deadlines de los proyectos, Jason Clarke menciona que por más que haya miedo e incertidumbre hacia los cambios a veces son necesarios para lograr grandes cosas, estos cambios podrían estar asociados a los hábitos en sí y de esta manera evitar la procrastinación. Steve Jobs de la misma manera menciona que hay que seguir los instintos, confiar en nosotros y en lo que queremos hacer, no gastar tiempo viviendo la vida de otro. Tener la valentía para abandonar la comodidad y afrontar lo desconocido.
+    </Typography>
+
+    <Box m={1}/>
+    <Typography variant="h5" component="h2" gutterBottom >
+      3. Relación entre Ken Robinson, Carole Cadwalladr y David Foster Wallace.
+    </Typography>
+
+    <Typography variant="subtitle1" gutterBottom align="justify"  >
+      Por un lado podemos relacionar la charla “Do schools kill creativity?” de Ken Robinson, con el discurso “This is Water” de David Foster Wallace, ya que ambos hablan de como tener una visión sobre el exterior. Ken menciona que se deben educar las capacidades creativas de los niños para que estos puedan enfrentar el futuro en materia ecológica y puedan ayudar al medio ambiente con sus ideas. De forma similar, David menciona en su discurso como las personas deben tener una mejor percepción sobre las pequeñas cosas y todo lo que se encuentra alrededor. 
+    </Typography>
+
+    <Typography variant="subtitle1" gutterBottom align="justify"  >
+      Por otro lado, podemos relacionar la charla “Facebook 's role in Brexit and the threat to democracy” de Carole Cadwalladr, con el discurso “This is Water” de David Forest Wallace. ya que ambos explican cómo las personas se quedan con un solo punto de vista. Carole explica cómo los habitantes de Ebbw Vale, en Gales, estaban hartos de no tener ayuda por parte de la Unión Europea y la problemática con los inmigrantes y refugiados, pero luego explica que esa ciudad tenía una de las tasas más bajas de inmigracion del país, por lo que menciona que el odio irracional de las personas era causado por lo que veían en Facebook. De igual forma, David en su discurso menciona cómo las personas tienen ese pensamiento por defecto que no las dejan pensar o ver más allá de lo que creen que es verdadero, en este caso la información que las personas obtenían de Facebook no era real y las mismas no trataban de investigar otra visión del problema..
+    </Typography>
+
+    <Typography variant="subtitle1" gutterBottom align="justify"  >
+      Por último, podemos relacionar la charla “Do schools kill creativity?” de Ken Robinson, con la charla “Facebook 's role in Brexit and the threat to democracy” de Carole Cadwalladr. Ya que lo relatado por Ken sobre la creatividad nos lleva a pensar que esta es una de las causas las cuales se crearon herramientas tan poderosas como Facebook, que utilizada de buena manera puede ser muy beneficiosa para la sociedad, pero cómo desarrolla Carole en su charla esta también puede ser utilizada de forma maligna, la cual mediante manipulaciones a las personas más persuasivas puede cambiar hasta el destino de una votación muy importante.
+    </Typography>
+
+    </Grid>
+    </>
+  )
+}
